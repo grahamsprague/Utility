aaf0da64625559ca2b3a78fa9ca7cf23c3f6b82f
grahamsprague <graham.sprague@gmail.com>
2018-02-22 11:26:58 -0500

    Created utility.php and an example page.

---
+++ example.php
@@ -0,0 +1,49 @@
+<?php
+	
+include ('utility.php');
+	
+$string = "I love PHP!";	
+$result = Utility::contains($string, 'PHP');
+echo '<pre>Contains<br>';
+echo '$string = "I love PHP!";<br />';
+echo '$result = Utility::contains($string, \'PHP\');';
+echo '</pre>';
+echo 'returns: '.$result;
+	
+$mypath = '/somedir/somedir/somedir/myfile.txt';
+$result = Utility::getFilenameWithExt ( $mypath );
+echo '<pre>Get filename with extension<br>';
+echo '$mypath = \'/somedir/somedir/somedir/myfile.txt\';<br />';
+echo '$result = Utility::getFilenameWithExt ( $filepath );';
+echo '</pre>';
+echo 'returns: '.$result;	
+	
+
+$mypath = '/somedir/somedir/somedir/myfile.txt';
+$result = Utility::getFilenameNoExt ( $mypath );
+echo '<pre>Get filename with out extension<br>';
+echo '$mypath = \'/somedir/somedir/somedir/myfile.txt\';<br />';
+echo '$result = Utility::getFilenameNoExt ( $filepath );';
+echo '</pre>';
+echo 'returns: '.$result;
+
+
+$mypath = '/somedir/somedir/somedir/myfile.txt';
+$result = Utility::getDirName ( $mypath );
+echo '<pre>Get directory name<br>';
+echo '$mypath = \'/somedir/somedir/somedir/myfile.txt\';<br />';
+echo '$result = Utility::getDirName ( $filepath );';
+echo '</pre>';
+echo 'returns: '.$result;
+
+$mypath = '/somedir/somedir/somedir/myfile.txt';
+$result = Utility::getDirPath ( $mypath );
+echo '<pre>Get directory path<br>';
+echo '$mypath = \'/somedir/somedir/somedir/myfile.txt\';<br />';
+echo '$result = Utility::getDirPath ( $filepath );';
+echo '</pre>';
+echo 'returns: '.$result;
+
+
+
+?>
---
+++ utility.php
@@ -0,0 +1,66 @@
+<?php
+	
+/**
+ * Provides a quick functions to aid with text manipulation
+*
+ * @package default
+ * @author Graham Sprague
+ */
+class Utility
+
+{
+	
+	public $string;
+	public $search;
+	public $filepath;
+	
+	public static function contains ( $string, $search ){
+		if ( substr($string, $search) !== false  ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public static function getFilenameWithExt ( $filepath ){
+		
+		$explodedPath = explode('/', $filepath );
+		
+		$result = $explodedPath[count($explodedPath)-1];
+		
+		return $result;
+		
+	}
+	
+	public static function getFilenameNoExt ( $filepath ){
+		
+		$explodedPath = explode('/', $filepath );
+		
+		$result = $explodedPath[count($explodedPath)-1];
+		
+		$explodedFilename = explode('.', $result);
+		
+		$result2 = $explodedFilename[count($explodedFilename)-2];
+		
+		return $result2;
+		
+	}
+	
+	public static function getDirName ( $filepath ){
+		
+		$explodedPath = explode('/', $filepath );
+		
+		$result = implode('/', array_slice($explodedPath, 0, count($explodedPath) - 2) );
+		
+		
+		
+		return $result;
+		
+	}
+	
+	
+} // END class 
+	
+	
+	
+?>
